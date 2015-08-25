f6989fd87cd725b187b01f10b3ab802ff08f31b2
Vinoth Energetic <vinothcse2000@gmail.com>
2015-08-25 11:11:42 +0530

    Create set-for-simuating-dupilate-values.cpp

---
+++ cpp-concepts/STL_CONTAINER/set-for-simuating-dupilate-values.cpp
@@ -0,0 +1,28 @@
+
+/* set for simuating dupilate values */
+
+#include <set>
+#include <string>
+#include <cstdio>
+
+int main()
+{
+   std::set <std::string> l_set;
+
+   l_set.insert(std::string("HAI"));
+   l_set.insert(std::string("HAI"));
+   l_set.insert(std::string("HAIiI"));
+   l_set.insert(std::string("HAIiI"));
+   l_set.insert(std::string("HAIiI"));
+   l_set.insert(std::string("HAIiI"));
+   l_set.insert(std::string("HAIiI"));
+   l_set.insert(std::string("HAIiI"));
+
+   std::set <std::string>::   iterator iter;
+
+   for(iter = l_set.begin(); iter != l_set.end(); iter++)
+   {
+      printf("\n  :: %s ", (*iter).c_str() );
+   }
+
+}
