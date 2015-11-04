56a3dd61ac664d3ee49c2344ad93ab5ddd7d98e1
Vinoth Energetic <vinothcse2000@gmail.com>
2015-11-04 18:30:52 +0530

    Create comment_line_in_perl.pl

---
+++ PERL-PROGRAMMING/comment_line_in_perl.pl
@@ -0,0 +1,18 @@
+print "\nSingle line comment";
+
+# This is single line comment in perl
+
+
+print "\nMulti line comment";
+
+=begin comment
+This is multi line comment
+
+This is multi line comment
+This is multi line comment
+= cut
+
+
+
+print "End:Multi line comment";
+
