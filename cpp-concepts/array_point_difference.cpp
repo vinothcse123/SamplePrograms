b756f830a451c69f9268e2e554ffd1e499fa0dee
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:36:51 +0530

    Create array_point_difference.cpp

---
+++ cpp-concepts/array_point_difference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main()
+{
+   using namespace std;
+
+   int arr[10];
+
+   int *poi_to_arr=arr; //pointer to array
+   int *arr_of_pointer[10]; // array of pointers
+
+
+
+   return 0;
+}
