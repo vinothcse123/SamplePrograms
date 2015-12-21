31dc3415df8638b4e80460b0ee9c625ef3515809
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:14:25 +0530

    Create perl_adding_and_removing_elements_in_array.pl

---
+++ PERL-PROGRAMMING/perl_adding_and_removing_elements_in_array.pl
@@ -0,0 +1,16 @@
+@v_array=(1,2,3);
+print("\n@v_array");
+
+push(@v_array,"Vinoth");
+print("\n@v_array");
+
+
+pop(@v_array);
+print("\n@v_array");
+
+shift(@v_array);
+print("\n@v_array");
+
+
+unshift(@v_array,"Saravana");
+print("\n@v_array");
