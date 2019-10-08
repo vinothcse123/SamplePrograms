49fba8e6bfcd578b0e5d445b4e6ffbc4097b4ab3
Vinoth Energetic <vinothcse2000@gmail.com>
2019-10-08 17:21:05 +0530

    Update SqlLoader.sql

--- Database/etc/SqlLoader.sql
+++ Database/etc/SqlLoader.sql
@@ -1,19 +1,20 @@
-$ORACLE_HOME/bin/sqlldr userid=${DB_CONNECTION_STRING} control=v6Play.ctl    direct=y errors=0 skip=0
+$ORACLE_HOME/bin/sqlldr userid=${DB_CONNECTION_STRING} control=v6Play.ctl    direct=y errors=0 skip=1
 
 v6Play.ctl
 LOAD DATA
 INFILE 'v6Play.dat'
 APPEND
-INTO TABLE MYSCHEMA.V6Play
+INTO TABLE MYSCHEMA .V6Play
 fields terminated by ","
 (
 MyNumber  ,
-myTimeStamp  DATE "dd-mon-yyyy"  ,
+myTimeStamp  DATE "yyyy-mm-dd hh24::mi:ss"  ,
 MyNumber2  
 )
 
 
 v6Play.dat
-10,31-JAN-2008,10000
+A,B,C
+1440,2019-10-03 15:47:00,10000
 
 
