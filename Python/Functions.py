e2ca01d627c1832ff5909f19ae754dc36ecc7b92
Vinoth Energetic <vinothcse2000@gmail.com>
2020-05-08 11:49:41 +0530

    Create Functions.py

---
+++ Python/Functions.py
@@ -0,0 +1,22 @@
+def myFunction():
+    print("I am simple function!")
+
+myFunction()
+
+
+
+#Function with arguments
+
+def myFuncWithArgs(myInt,myString):
+    print("My Int is %d and string is %s"%(myInt,myString))
+
+myFuncWithArgs(100,"Vinoth");
+
+
+
+#Function with return value
+
+def sumMyNumbers(a,b):
+    return a+b
+
+print(sumMyNumbers(100,200))
