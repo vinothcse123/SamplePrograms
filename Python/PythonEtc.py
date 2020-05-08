e5daaf473e0e98dc5ce6e96cc43443442eb59b93
Vinoth Energetic <vinothcse2000@gmail.com>
2020-05-08 11:48:35 +0530

    Create PythonEtc.py

---
+++ Python/PythonEtc.py
@@ -0,0 +1,17 @@
+#is operator
+
+a=10
+b=a
+c=20
+
+if a is b:
+    print("Yeah a is b")
+if a is c:
+    print("Yeah a is c")
+else:
+    print("No a is not c")
+
+# not operator
+a=4
+if not a == 10:
+    print("Yeah")
