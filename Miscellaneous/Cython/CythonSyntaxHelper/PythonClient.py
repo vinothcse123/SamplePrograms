3f2a61521f5d3c74050298aef3622c83afe355b0
Vinoth Energetic <vinothcse2000@gmail.com>
2020-06-12 12:26:08 +0530

    Create PythonClient.py

---
+++ Miscellaneous/Cython/CythonSyntaxHelper/PythonClient.py
@@ -0,0 +1,32 @@
+import CythonFunctions
+
+CythonFunctions.directPythonFunc();
+CythonFunctions.pythonCallingCFunc();
+CythonFunctions.sameFunctionNameinBoth();
+CythonFunctions.FunctionWithArgs("IamFromPython");
+CythonFunctions.FunctionWithVector([10,20]);
+
+
+#Accessing C types in python
+myPythonObject = CythonFunctions.returnPythonClass()
+print(myPythonObject.myPythonVar)
+print(myPythonObject.myInt)
+CythonFunctions.takePythonObjectAndAccessCType(myPythonObject)
+
+CythonFunctions.convertToCppEnum(CythonFunctions.MyPythonEnum.MyPythonEnumValue1)
+
+CythonFunctions.myGenericFuncion(100)
+CythonFunctions.myGenericFuncion(89.0002)
+CythonFunctions.myGenericFuncion("myString")
+
+
+CythonFunctions.LoopingAllElementsInList([98,86])
+
+CythonFunctions.passingCppClassToCppProgram(100,[["newString","two"]])
+CythonFunctions.testingMap(CythonFunctions.PyColumnType.eBOOL)
+
+#CythonFunctions.myCdefFuncion() Cannot be called
+CythonFunctions.defFuncUsingInt()
+
+print(CythonFunctions.returnBooleanValue())
+
