9619e0947ba4e8f3974ecaf0bb2204dbfbfbdc2c
Vinoth Energetic <vinothcse2000@gmail.com>
2020-09-21 16:26:45 +0530

    Update ociDbConnect.cpp

--- Database/oci/ociDbConnect.cpp
+++ Database/oci/ociDbConnect.cpp
@@ -1,89 +1,113 @@
 
 //g++ $1 -std=c++11 -I $ORACLE_HOME/rdbms/public/ -L $ORACLE_HOME/lib/  -lclntsh
 
-#include<iostream>
+#include <iostream>
 #include <oci.h>
 #include <string>
+#include <cstring>
 
-void handleError(OCIError* pOciErrorHandle)
+using namespace std;
+
+class OCIConnection
+{
+public:
+	string schemaNameStr = "schemaNameStr", userStr = "schemaNameStr", passwordStr = "schemaNameStr";
+	OCIEnv *pOciEnv;
+	OCIError *pOciErrorHandle;
+	OCISvcCtx *pOciServiceContextHandle = nullptr;
+	sword ociret; /* return code from OCI calls */
+	ub2 id_col;
+	OCIServer *pOciServerHandle = nullptr;
+	OCISession *pOciSessionHandle = nullptr;
+
+	int connectToDb();
+};
+
+void handleError(OCIError *pOciErrorHandle)
 {
 	sb4 errcode = 0;
-		text errorBuf[512];	
-		OCIErrorGet((dvoid *)pOciErrorHandle, (ub4)1, (text *)NULL, &errcode, errorBuf, (ub4) sizeof(errorBuf), OCI_HTYPE_ERROR);
-		std::cout << " \n==============OCI ERROR: " <<  (const char*)errorBuf << std::endl;
+	text errorBuf[5120];
+	sword errorCode = OCIErrorGet((dvoid *)pOciErrorHandle, (ub4)1, (text *)NULL, &errcode, errorBuf, (ub4)sizeof(errorBuf), OCI_HTYPE_ERROR);
+
+	if (OCI_ERROR != errorCode)
+		std::cout << " \n==============OCI ERROR: " << errcode << " : " << (const char *)errorBuf << std::endl;
+	else
+		std::cout << " \n==============Error in getting OCI ERROR: " << errorCode << (const char *)errorBuf << std::endl;
+
+	exit(-1);
 }
 
-int main()
+void IS_ERROR(OCIError *pOciErrorHandle, sword errorCode)
 {
-	using namespace std;
-	string schemaNameStr="MYSCHEMANAME",userStr="USER_NAME",passwordStr="PASSWORD";
 
-    OCIEnv* pOciEnv=nullptr;
-	OCIServer* pOciServerHandle=nullptr;	
-	OCISvcCtx* pOciServiceContextHandle=nullptr;
-	OCISession* pOciSessionHandle=nullptr;
-	OCIError* pOciErrorHandle=nullptr;
-
-    //Create Environment
-
-    if (OCIEnvCreate((OCIEnv **)&pOciEnv, (ub4)OCI_DEFAULT,
-		(dvoid *)0, (dvoid * (*)(dvoid *, size_t)) 0,
-		(dvoid * (*)(dvoid *, dvoid *, size_t)) 0,
-		(void(*)(dvoid *, dvoid *)) 0, (size_t)0, (dvoid **)0))
+	if (errorCode != OCI_SUCCESS)
 	{
-        std::cout << " \n==============V6P: Unabled to create Environment==================" << std::endl;
+		std::cout << " \n==============Returned error Code: " << errorCode << std::endl;
+		if (errorCode == OCI_ERROR)
+			handleError(pOciErrorHandle);
+	}
+}
+
+int OCIConnection::connectToDb()
+{
+	//Create Environment
+
+	if (OCIEnvCreate((OCIEnv **)&pOciEnv, (ub4)OCI_DEFAULT,
+					 (dvoid *)0, (dvoid * (*)(dvoid *, size_t))0,
+					 (dvoid * (*)(dvoid *, dvoid *, size_t))0,
+					 (void (*)(dvoid *, dvoid *))0, (size_t)0, (dvoid **)0))
+	{
+		std::cout << " \n==============V6P: Unabled to create Environment==================" << std::endl;
 		return -1;
 	}
 
 	// Set error handler
 
-	if(OCI_SUCCESS != OCIHandleAlloc((dvoid *)pOciEnv, (dvoid **)&pOciErrorHandle, OCI_HTYPE_ERROR, (size_t)0, (dvoid **)0))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCIHandleAlloc((dvoid *)pOciEnv, (dvoid **)&pOciErrorHandle, OCI_HTYPE_ERROR, (size_t)0, (dvoid **)0));
 
 	// set server handle
 
-	if(OCI_SUCCESS != OCIHandleAlloc((dvoid *)pOciEnv, (dvoid **)&pOciServerHandle, OCI_HTYPE_SERVER, (size_t)0, (dvoid **)0))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCIHandleAlloc((dvoid *)pOciEnv, (dvoid **)&pOciServerHandle, OCI_HTYPE_SERVER, (size_t)0, (dvoid **)0));
 
 	//set service handle
 
-	if(OCI_SUCCESS != OCIHandleAlloc((dvoid *)pOciEnv, (dvoid **)&pOciServiceContextHandle, OCI_HTYPE_SVCCTX, (size_t)0, (dvoid **)0))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCIHandleAlloc((dvoid *)pOciEnv, (dvoid **)&pOciServiceContextHandle, OCI_HTYPE_SVCCTX, (size_t)0, (dvoid **)0));
 
 	//Connect to server
 
-	if(OCI_SUCCESS != OCIServerAttach(pOciServerHandle, pOciErrorHandle, (text *)schemaNameStr.c_str(), (sb4)schemaNameStr.length(), (ub4)OCI_DEFAULT))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCIServerAttach(pOciServerHandle, pOciErrorHandle, (text *)schemaNameStr.c_str(), (sb4)schemaNameStr.length(), (ub4)OCI_DEFAULT));
 
 	//set server context
 
-	if(OCI_SUCCESS != OCIAttrSet((dvoid *)pOciServiceContextHandle, (ub4)OCI_HTYPE_SVCCTX, (dvoid *)pOciServerHandle, (ub4)0, (ub4)OCI_ATTR_SERVER, (OCIError *)pOciErrorHandle))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCIAttrSet((dvoid *)pOciServiceContextHandle, (ub4)OCI_HTYPE_SVCCTX, (dvoid *)pOciServerHandle, (ub4)0, (ub4)OCI_ATTR_SERVER, (OCIError *)pOciErrorHandle));
 
 	//Allocate session handle
-
-	if(OCI_SUCCESS != OCIHandleAlloc((dvoid *)pOciEnv, (dvoid **)&pOciSessionHandle, (ub4)OCI_HTYPE_SESSION, (size_t)0, (dvoid **)0))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCIHandleAlloc((dvoid *)pOciEnv, (dvoid **)&pOciSessionHandle, (ub4)OCI_HTYPE_SESSION, (size_t)0, (dvoid **)0));
 
 	// Set the username
-	if(OCI_SUCCESS != OCIAttrSet((dvoid *)pOciSessionHandle, (ub4)OCI_HTYPE_SESSION, (dvoid *)userStr.c_str(), (ub4)userStr.length(), (ub4)OCI_ATTR_USERNAME, pOciErrorHandle))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCIAttrSet((dvoid *)pOciSessionHandle, (ub4)OCI_HTYPE_SESSION, (dvoid *)userStr.c_str(), (ub4)userStr.length(), (ub4)OCI_ATTR_USERNAME, pOciErrorHandle));
 
 	// Set the password
-	if(OCI_SUCCESS != OCIAttrSet((dvoid *)pOciSessionHandle, (ub4)OCI_HTYPE_SESSION, (dvoid *)passwordStr.c_str(), (ub4)passwordStr.length(), (ub4)OCI_ATTR_PASSWORD, pOciErrorHandle))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCIAttrSet((dvoid *)pOciSessionHandle, (ub4)OCI_HTYPE_SESSION, (dvoid *)passwordStr.c_str(), (ub4)passwordStr.length(), (ub4)OCI_ATTR_PASSWORD, pOciErrorHandle));
 
 	// Set the schema attribute
-	if(OCI_SUCCESS != OCIAttrSet((dvoid *)pOciSessionHandle, (ub4)OCI_HTYPE_SESSION, (dvoid *)userStr.c_str(), (ub4)userStr.length(), (ub4)OCI_ATTR_CURRENT_SCHEMA, pOciErrorHandle))
-		handleError(pOciErrorHandle);
-
+	IS_ERROR(pOciErrorHandle, OCIAttrSet((dvoid *)pOciSessionHandle, (ub4)OCI_HTYPE_SESSION, (dvoid *)userStr.c_str(), (ub4)userStr.length(), (ub4)OCI_ATTR_CURRENT_SCHEMA, pOciErrorHandle));
 
 	//Begin the session
-	if(OCI_SUCCESS != OCISessionBegin(pOciServiceContextHandle, pOciErrorHandle, pOciSessionHandle, OCI_CRED_RDBMS, (ub4)OCI_DEFAULT))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCISessionBegin(pOciServiceContextHandle, pOciErrorHandle, pOciSessionHandle, OCI_CRED_RDBMS, (ub4)OCI_DEFAULT));
 
 	// set session parameters for authentication
-	if(OCI_SUCCESS != OCIAttrSet((dvoid *)pOciServiceContextHandle, (ub4)OCI_HTYPE_SVCCTX, (dvoid *)pOciSessionHandle, (ub4)0, (ub4)OCI_ATTR_SESSION, pOciErrorHandle))
-		handleError(pOciErrorHandle);
+	IS_ERROR(pOciErrorHandle, OCIAttrSet((dvoid *)pOciServiceContextHandle, (ub4)OCI_HTYPE_SVCCTX, (dvoid *)pOciSessionHandle, (ub4)0, (ub4)OCI_ATTR_SESSION, pOciErrorHandle));
+
+	return 0;
+}
+
+
+int main()
+{
+	OCIConnection ociConnection;
+
+	if (0 != ociConnection.connectToDb())
+		return -1;
 
 }
