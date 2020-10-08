9685a01648cd7b582fa789b4803c9d617b00d026
Vinoth Energetic <vinothcse2000@gmail.com>
2020-10-08 13:46:33 +0530

    Update SyntaxHelper.sql

--- Database/oracle/SyntaxHelper.sql
+++ Database/oracle/SyntaxHelper.sql
@@ -1,1 +1,21 @@
--- Index creation
+--H:Table
+
+create table V6Play
+(
+MyNumber NUMBER,
+MyVarchar VARCHAR2(200),
+myDate date,
+myTimeStamp timestamp,
+MyNumber2 NUMBER
+);
+
+ALTER TABLE V6Play add MyNumber3 NUMBER;
+
+DELETE FROM V6Play;
+SELECT * FROM V6Play;
+INSERT INTO V6Play(MYDATE) VALUES(to_date('10-DEC-21 10:34:33','yyyy/mm/dd hh24:mi:ss'));
+
+
+--H:Index
+
+create unique index V6PlayUniqueIdx ON V6Play(MyNumber);
