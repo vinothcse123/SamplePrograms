b3cc2cff02f4f114599c232747d1e719f43f11de
Vinoth Energetic <vinothcse2000@gmail.com>
2020-10-08 14:06:26 +0530

    Update SyntaxHelper.sql

--- Database/oracle/SyntaxHelper.sql
+++ Database/oracle/SyntaxHelper.sql
@@ -19,3 +19,12 @@ INSERT INTO V6Play(MYDATE) VALUES(to_date('10-DEC-21 10:34:33','yyyy/mm/dd hh24:
 --H:Index
 
 create unique index V6PlayUniqueIdx ON V6Play(MyNumber);
+
+--rebuild the index
+ALTER INDEX V6PlayUniqueIdx REBUILD;
+
+-- make index as UNUSABLE - STATUS COLUMN IN all_index
+alter index V6PlayUniqueIdx unusable;
+--Instructing to skip unusable index
+alter session set skip_unusable_indexes = true;
+
