3a31e99f4767625b7ac7213b75f485e1fc6f27c1
Vinoth Energetic <vinothcse2000@gmail.com>
2020-10-14 12:10:24 +0530

    Update connectToDb.cpp

--- Database/odpi/connectToDb.cpp
+++ Database/odpi/connectToDb.cpp
@@ -1,30 +1,43 @@
 
-#include<iostream>
+#include <iostream>
 #include <dpi.h>
 #include <cstring>
+#include <string>
+
+#define MAJOR_VERSION 4
+#define MINOR_VERSION 1
+
+using std::string;
+
+dpiContext *pContext = NULL;
+dpiConn *conn;
+
+void connectToDb()
+{
+    string userName = "userName", password = "userName", dbName = "userName";
+    dpiErrorInfo errorInfo;
+
+    if (dpiContext_createWithParams(MAJOR_VERSION, MINOR_VERSION,
+                                    NULL, &pContext, &errorInfo) < 0)
+    {
+        fprintf(stderr, "ERROR: %.*s (%s : %s)\n", errorInfo.messageLength,
+                errorInfo.message, errorInfo.fnName, errorInfo.action);
+    }
+
+    if (dpiConn_create(pContext, userName.c_str(),
+                       userName.length(), password.c_str(),
+                       password.length(), dbName.c_str(),
+                       dbName.length(), NULL, NULL, &conn) < 0)
+    {
+        dpiErrorInfo info;
+        dpiContext_getError(pContext, &info);
+        fprintf(stderr, "ERROR: %.*s (%s: %s), offset: %u\n", info.messageLength,
+                info.message, info.fnName, info.action, info.offset);
+    }
+}
 
 int main()
 {
-    dpiContext *gContext = NULL;
-    dpiErrorInfo errorInfo;
-    dpiConn *conn;
-
-    if (dpiContext_createWithParams(4, 1,
-                NULL, &gContext, &errorInfo) < 0) {
-            fprintf(stderr, "ERROR: %.*s (%s : %s)\n", errorInfo.messageLength,
-                    errorInfo.message, errorInfo.fnName, errorInfo.action);
-        }
-
-    if (dpiConn_create(gContext, "string",
-            strlen("string"),"string",
-            strlen("string"), "string",
-            strlen("string"), NULL, NULL, &conn) < 0) {
-                dpiErrorInfo info;
-        dpiContext_getError(gContext, &info);
-         fprintf(stderr, "ERROR: %.*s (%s: %s), offset: %u\n", info.messageLength,
-            info.message, info.fnName, info.action, info.offset);
-    }
-
+    connectToDb();
     return 0;
-
 }
