3ebaa6e50b1ef87eda4d0c7d9f3d7c6e5a07db8b
Vinoth Energetic <vinothcse2000@gmail.com>
2020-10-21 16:03:34 +0530

    Update TemplateSyntaxHelper.cpp

--- cpp-concepts/TemplateSyntaxHelper.cpp
+++ cpp-concepts/TemplateSyntaxHelper.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 template <class type1> 
-class arithmetic_oper; //declaring template
+class TemplateClass; //declaring template
 
 template <class type1>
-class arithmetic_oper //defining template
+class TemplateClass 
 {
    public:
       type1 variable_name; //Here we can change the datatype
@@ -16,22 +16,17 @@ class arithmetic_oper //defining template
 
       //Using same type as class for function template
       type1 sub(type1,type1);
-
 };
+
 
 template <class c_Type1>
 template <class type1>
-type1 arithmetic_oper<c_Type1> ::  add(type1 a,type1 b)
+type1 TemplateClass<c_Type1> ::  add(type1 a,type1 b)
 {
    return a+b;
 }
 
 
-template <class c_Type1>
-c_Type1 arithmetic_oper<c_Type1> :: sub(c_Type1 a,c_Type1 b)
-{
-   return a-b;
-}
 
 template<class T>
 void callMemberFunctionOfUnknownType(T type)
@@ -41,42 +36,23 @@ void callMemberFunctionOfUnknownType(T type)
 }
 
 
+
 int main()
 {
 
    //Creating object as int using template
-   arithmetic_oper <int> int_operation;
-   int_operation.variable_name=10;
-   printf("\nUsing a class as a integer :: %d ",int_operation.variable_name);
+   TemplateClass <int> int_operation;
+   int_operation.variable_name=10;   
    int addition_value=int_operation.add(5,10); 
-   printf("\nADDITION OF GIVEN VALUE IS %d",addition_value);
    addition_value=(int)int_operation.add<int>(5.10,10); // Type casting as int.
-   printf("\nADDITION OF GIVEN VALUE IS with type casting %d",addition_value);
 
-
-   //Creating object as double using template
-   arithmetic_oper <double> double_operation;
-   double sub_value=double_operation.sub(50.52,25.02);
-   printf("\nSUBTRACTION OF GIVEN VALUE IS %f",sub_value);
-
-   //Creating object as char using template
-   arithmetic_oper <char> char_operation;
-   char_operation.variable_name='A';
-   printf("\nUsing a class as char type :: %c ",char_operation.variable_name);
 
    //passing template and calling member function of that
    callMemberFunctionOfUnknownType(int_operation);
 
-
+   //Pointer to template class
+   TemplateClass<int>* ptr=new TemplateClass<int>;
 
    return 0;
 }
-/*
-OUTPUT:
-Using a class as a integer :: 10 
-ADDITION OF GIVEN VALUE IS 15
-ADDITION OF GIVEN VALUE IS with type casting 15
-SUBTRACTION OF GIVEN VALUE IS 25.500000
-Using a class as char type :: A 
 
-*/
