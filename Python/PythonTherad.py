2ef7a3472fb267b307eecaa86e579e6b4189f5a9
Vinoth Energetic <vinothcse2000@gmail.com>
2020-11-05 11:04:20 +0530

    Update PythonTherad.py

--- Python/PythonTherad.py
+++ Python/PythonTherad.py
@@ -1,14 +1,26 @@
+
 import threading
+import time
+
+
+## Without class model
 
 # Define a function for the thread
 def threadRun( threadName):
     print("%s" % ( threadName ))
+    time.sleep(2.4)
+    print("End of threadRun")
 
 # Create two threads as follows
 try:
    x = threading.Thread(target=threadRun, args=("MyThread",))
    x.start()
+   print("Main Thread processing...")
+   x.join()
+   print("Main Thread ends")
 except:
    print("Error: unable to start thread")
 
 
+#With class type
+#https://www.geeksforgeeks.org/joining-threads-in-python/
