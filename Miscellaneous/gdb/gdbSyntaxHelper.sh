55ab3dbf937ce6691fa69d9a57d0e2ebf82d827d
Vinoth Energetic <vinothcse2000@gmail.com>
2020-11-17 23:43:48 +0530

    Update gdbSyntaxHelper.sh

--- Miscellaneous/gdb/gdbSyntaxHelper.sh
+++ Miscellaneous/gdb/gdbSyntaxHelper.sh
@@ -1,7 +1,11 @@
+#Master gdb starting command
+gdb -q -x v6gdbInit.gdb --args ./myProgram arg1 arg2 
+
+
 #passing arguments
 gdb --args ./myProgram arg1 arg2
 
 
 #Run default commands at startup
-gdb -x gdbInit.ksh --args ./myProgram arg1 arg2
+gdb -x v6gdbInit.gdb --args ./myProgram arg1 arg2
 
