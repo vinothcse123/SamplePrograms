14362d2d0ee4b8087b4fd9b6575e3a88a81a3980
Vinoth Energetic <vinothcse2000@gmail.com>
2020-12-08 11:05:04 +0530

    Update index.sql

--- Database/oracle/index.sql
+++ Database/oracle/index.sql
@@ -1,3 +1,16 @@
+--H:Index
+
+create unique index V6PlayUniqueIdx ON V6Play(MyNumber);
+
+--rebuild the index
+ALTER INDEX V6PlayUniqueIdx REBUILD;
+
+-- make index as UNUSABLE - STATUS COLUMN IN all_index, To enable again, rebuild index
+alter index V6PlayUniqueIdx unusable;
+
+--Instructing to skip unusable index
+
+
 -- partioned index
 
 -- LOCAL :  It creates partition index on table with partition info provided while creating table
