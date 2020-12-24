0a58af081266afca2c8e0919881e3a1c55ae1df2
Vinoth Energetic <vinothcse2000@gmail.com>
2020-12-24 17:40:44 +0530

    Create any.cpp

---
+++ cpp-concepts/any.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <any>
+
+using std::cout;
+using std::string;
+using std::vector;
+
+int main()
+{
+    std::any a, b;
+
+    vector<int> vec = {100, 44, 3};
+    a = vec;
+    vector<string> stringVec = {"asdf3"};
+    b = stringVec;
+
+    vector<std::any> anyVec;
+
+    anyVec.push_back(a);
+    anyVec.push_back(b);
+
+    for (auto x : std::any_cast<vector<int>>(a))
+        cout << "Result :" << x << '\n';
+
+    return 0;
+}
