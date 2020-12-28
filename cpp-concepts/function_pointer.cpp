d7f1877029dbbd8555abe488f5690cc295bef90c
Vinoth Energetic <vinothcse2000@gmail.com>
2020-12-28 18:29:28 +0530

    Update function_pointer.cpp

--- cpp-concepts/function_pointer.cpp
+++ cpp-concepts/function_pointer.cpp
@@ -17,6 +17,20 @@ int operationfunc(int first, int second, int (*functocall)(int, int))
    return (*functocall)(first, second);
 }
 
+class MyClass
+{
+    public:
+    int m_var=-1;
+
+    void myMemberFunction()
+    {
+        cout << "m_var = " << m_var << endl;
+
+    }
+
+    
+};
+
 int main()
 {
    int  a, b;
@@ -26,5 +40,14 @@ int main()
    a = operationfunc(7, 5, plus);
    b = operationfunc(20, a, minus);
    cout << "a = " << a << " and b = " << b << endl;
+
+   
+    MyClass myObj;
+    void (MyClass::*pointerToMemberFunc)();
+    
+   pointerToMemberFunc = &MyClass::myMemberFunction;
+
+   (myObj.*pointerToMemberFunc)();
+
    return 0;
 }
