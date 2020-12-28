966d797fc24c6dfb9b572e5014d1e7f31d1e80c3
Vinoth Energetic <vinothcse2000@gmail.com>
2020-12-28 10:59:14 +0530

    Create valueCategory.cpp

---
+++ cpp-concepts/valueCategory.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::vector;
+using std::cout;
+using std::string;
+
+void lvalueCategory()
+{
+    int a=10;
+    
+}
+
+void rvalueCategory()
+{
+    //myFunction(10);
+    
+}
+
+int main()
+{
+    int myNumber=-1;
+    lvalueCategory();
+
+    cout<< "Result :" << myNumber << '\n';	
+    return 0;
+}
