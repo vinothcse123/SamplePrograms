dd5f346499955e6c9767ab54a3b6cbdf69f38efb
Vinoth Energetic <vinothcse2000@gmail.com>
2021-01-05 10:42:13 +0530

    Update Array.cpp

--- cpp-concepts/Array.cpp
+++ cpp-concepts/Array.cpp
@@ -1,23 +1,43 @@
 #include<iostream>
 
+
 int main()
 {
    using namespace std;
 
-   /* Array same address for normal array and &array
+   /* H1 : Address related in array */
    int a,*p;
    p=&a;
    printf("\n p :: %p ", p  );
    printf("\n &p  :: %p ",&p  );
    
-   
-   
-   /*Two dimensonal Array */
 
+
+
+    /* H2: Two dimensional Array */
+
+
+   /* S1: Static Allocation of two dimensional array */
+
+   /* Below one is 10 rows and 5 columns */ 
    int array2d[10][5];
-   display(array2d);
+
+   int dynamicSizeof[][5]={10,5};
 
 
+   /* S2: Dynamic Allocation of two dimensional array */
+    
+    const int ROW_COUNT=10;
+    const int COL_COUNT=5;
+    int **twoDimensonArray = new int*[ROW_COUNT];
+
+    for(int i=0; i<ROW_COUNT; i++)
+    {
+        //Here, as like static array each row is not continuous with another row
+        twoDimensonArray[i] = new int[COL_COUNT];
+        *(twoDimensonArray[i])= i;
+    }
+   
 
 
    return 0;
@@ -25,7 +45,3 @@ int main()
 
 
 
-void display(int **array2d)
-{
-   printf("/n Called");
-}
