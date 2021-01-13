0812dfdbd531499270d61a70797c157ec9c414d1
Vinoth Energetic <vinothcse2000@gmail.com>
2021-01-13 18:38:36 +0530

    Update Table.sql

--- Database/oracle/Table.sql
+++ Database/oracle/Table.sql
@@ -1,3 +1,9 @@
+
 --Global Temporary table
+
+-- Temporary table will not be dropped at end of session, where as data inserted in one session is not visible to others.
 CREATE GLOBAL TEMPORARY TABLE V6Play(myNumber NUMBER)
-ON COMMIT PRESERVE ROWS;
+ON COMMIT PRESERVE ROWS; -- While commiting data will be saved to temporary table
+
+CREATE GLOBAL TEMPORARY TABLE V6Play(myNumber NUMBER)
+ON COMMIT DELETE  ROWS; -- While commiting data will be deleted from temporary table
