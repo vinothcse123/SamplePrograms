72b7bf73ebb9ef2b26c87083bbdb2840067445f3
Vinoth Energetic <vinothcse2000@gmail.com>
2021-01-21 15:22:19 +0530

    Update DateTime.cpp

--- cpp-concepts/DateTime.cpp
+++ cpp-concepts/DateTime.cpp
@@ -3,9 +3,20 @@
 #include<iostream>
 #include<cstdio>
 #include<ctime> // In c it is <time.h>
+#include<chrono> 
+#include<thread> 
 
+void printTimeStruct(tm *time_struct)
+{
+  printf("\n CURRENT YEAR :: %d ",  (time_struct->tm_year+1900)     );
+   printf("\n CURRENT MON  :: %d ",  (time_struct->tm_mon+1)     );
+   printf("\n CURRENT DAY  :: %d ",  time_struct->tm_mday     );
+   printf("\n CURRENT HOUR :: %d ",  time_struct->tm_hour     );
+   printf("\n CURRENT MIN  :: %d ",  time_struct->tm_min     );
+   printf("\n CURRENT SEC  :: %d ",  time_struct->tm_sec     );
+}
 
-void getCurrentTimeAndAlterTime()
+void getCurrentTimeUsingEpoch()
 {
    using namespace std;
 
@@ -15,21 +26,15 @@ void getCurrentTimeAndAlterTime()
 
    time(&last_connect_time);
    // -1 will be returned if not able to fetch caledar time
-   sleep(1);
+   std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // sleep for 1 second
    current_time=time(0); // This is similar to time(&current_time)
    printf("\n DIFFERENCE TIME IN SECONDS  :: %f ", difftime(current_time,last_connect_time) );
 
    printf("\n Epoch time or unix time     :: %ld ",         current_time );
 
    time_struct = localtime(&current_time);
-   printf("\n CURRENT YEAR :: %d ",  (time_struct->tm_year+1900)     );
-   printf("\n CURRENT MON  :: %d ",  (time_struct->tm_mon+1)     );
-   printf("\n CURRENT DAY  :: %d ",  time_struct->tm_mday     );
-   printf("\n CURRENT HOUR :: %d ",  time_struct->tm_hour     );
-   printf("\n CURRENT MIN  :: %d ",  time_struct->tm_min     );
-   printf("\n CURRENT SEC  :: %d ",  time_struct->tm_sec     );
+   printTimeStruct(time_struct);
 
-   printf("\n mk_time to get epoch time again  :: %ld ",    mktime(time_struct) );
 
    printf("\n asctime       :: %s ",     asctime(time_struct)     );
    strftime(formatted_time,sizeof(formatted_time),"%d-%b-%y",time_struct);
@@ -37,10 +42,23 @@ void getCurrentTimeAndAlterTime()
 
 }
 
+void prepareTimeAndConvertToEpoch()
+{
+    struct tm *time_struct;
+    time_t currentTime=time(0);
+    time_struct = localtime(&currentTime);
+    time_struct->tm_year = 2020 - 1900;
+    time_struct->tm_mon = 1 - 1;
+    time_struct->tm_mday = 5;
+
+    printf("\n mk_time to get epoch time again  :: %ld ",    mktime(time_struct) );
+    printTimeStruct(time_struct);
+}
+
 int main()
 {
-   getCurrentTimeAndAlterTime();
-
+   getCurrentTimeUsingEpoch();
+   prepareTimeAndConvertToEpoch(); 
 
    return 0;
 }
