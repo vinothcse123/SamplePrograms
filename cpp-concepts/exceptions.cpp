28f6d9c394e317659b53477c18f266bab868f858
Vinoth Energetic <vinothcse2000@gmail.com>
2021-01-29 16:23:49 +0530

    Update exceptions.cpp

--- cpp-concepts/exceptions.cpp
+++ cpp-concepts/exceptions.cpp
@@ -2,6 +2,49 @@
 
 #include<iostream>
 #include <exception>
+#include <thread>
+#include <iostream>
+#include <future>
+#include <mutex>
+using namespace std;
+#include <condition_variable> // std::condition_variable
+
+std::mutex g_mutex;
+std::condition_variable cv;
+int g_int=10;
+bool g_isCondWaited=false;
+
+void threadModule(int myInput)
+{
+    std::cout << "Thread Id: " << std::this_thread::get_id() << std::endl;
+
+
+    throw "throwing exception in worker thread";
+  
+    
+}
+
+void threadModuleCondWait(int myInput)
+{
+    
+    std::unique_lock<std::mutex> lck(g_mutex);
+        
+    if(!g_isCondWaited)
+    {
+        g_isCondWaited=true;
+        std::cout << "Going to cond wait" << std::this_thread::get_id() << std::endl;
+        cv.wait(lck);        
+        
+    }
+    else
+    {
+        std::cout << "Throwing exception" << std::this_thread::get_id() << std::endl;
+        //Before throwing exception, notify conditional waits to avoid blocking
+         throw "throwing exception in worker thread";
+    }
+}
+
+
 
 
 class Divide_by_zero : public std::exception
@@ -28,13 +71,76 @@ void userDefinedException()
    }
 }
 
-using namespace std;
+
+void exceptionInThreads()
+{
+    try
+    {
+
+    
+
+    using namespace std;
+    std::thread threadObj(&threadModule, 100);
+    threadObj.join();
+
+    std::thread threadArray[3];
+    for (int i = 0; i < 3; i++)
+    {
+        threadArray[i] = thread(&threadModule, 100);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        threadArray[i].join();
+    }
+
+    std::cout << "All thread completed" << std::endl;
+
+    }
+    catch(...)
+    {
+        std::cout << "Exception in main thread" << std::endl;
+    }
+
+}
+
+void exceptionInCondWaitThreads()
+{
+    try
+    {
+
+    
+
+    using namespace std;
+
+    std::thread threadArray[3];
+    for (int i = 0; i < 3; i++)
+    {
+        threadArray[i] = thread(&threadModuleCondWait, 100);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        threadArray[i].join();
+    }
+
+    std::cout << "All thread completed" << std::endl;
+
+    }
+    catch(...)
+    {
+        std::cout << "Exception in main thread" << std::endl;
+    }
+
+}
+
 
 int main()
 {
    
-   userDefinedException();
-
+   //userDefinedException();
+    //exceptionInThreads();
+    exceptionInCondWaitThreads();
 
 
    return 0;
