c66051ffce54829f04a44c303f6129958c763dde
Vinoth Energetic <vinothcse2000@gmail.com>
2021-02-08 13:22:28 +0530

    Create PythonClient.py

---
+++ Miscellaneous/Cython/PassingCythonOracleConToCppOci/PythonClient.py
@@ -0,0 +1,12 @@
+#GH_MISC_4
+import CythonFunctions
+import cx_Oracle
+
+# Establish the database connection
+connection = cx_Oracle.connect("NRSP_V", "NRSP_V", "VALDEV03")
+
+# Obtain a cursor
+cursor = connection.cursor()
+
+print(connection.handle)
+CythonFunctions.FunctionWithArgs(connection.handle);
