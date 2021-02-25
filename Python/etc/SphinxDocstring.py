b8f3e7ec519829c62da60d763115a2faecb7d1bd
Vinoth Energetic <vinothcse2000@gmail.com>
2021-02-25 11:15:43 +0530

    Create SphinxDocstring.py

---
+++ Python/etc/SphinxDocstring.py
@@ -0,0 +1,23 @@
+def myFunction():
+"""
+    v6pDescription.
+ 
+    Parameters:
+        query(str): Oracle query which is used for immediate execution. 
+ 
+    Returns:
+        int: Identifier of the node
+        None
+ 
+    Note:
+        Mynote goes here.
+ 
+    Example:
+        .. code-block:: python
+            createMyNode(1)
+            printNode(1)
+ 
+    Version : 21.2
+  
+"""
+print("Hello")
