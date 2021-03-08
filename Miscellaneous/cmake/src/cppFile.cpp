50a3d11225a29e2e3f67ff927c32d8f2a5e7aa6c
Vinoth Energetic <vinothcse2000@gmail.com>
2021-03-08 11:36:23 +0530

    Create cppFile.cpp

---
+++ Miscellaneous/cmake/src/cppFile.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::vector;
+using std::cout;
+using std::string;
+
+int main()
+{
+    int myNumber=-1;
+
+
+    cout<< "Result :" << myNumber << '\n';	
+    return 0;
+}
