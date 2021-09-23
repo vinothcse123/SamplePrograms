e6bad8e90bd46cda82f168adcba2788f1aee75ee
Vinoth Energetic <vinothcse2000@gmail.com>
2021-09-23 14:37:15 +0530

    Create filesystem.cpp

---
+++ cpp-concepts/filesystem.cpp
@@ -0,0 +1,15 @@
+//GH_CPP_CON_14
+// Example program
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+int main()
+{
+ std::filesystem::path obj= "/vinoth/playground/testme/dfd.txt";
+  std::cout << "Hello, " << obj.parent_path() << "!\n";
+
+  if(!std::filesystem::exists(obj))
+    std::filesystem::create_directory(obj.parent_path()); //add directories based on the object path (without this line it will not work)
+
+}
