8f3a1f21a7ad8915b7f8d1647483ebd9f5ba4d1c
Vinoth Energetic <vinothcse2000@gmail.com>
2021-11-30 15:02:50 +0530

    Update gdbSyntaxHelper.sh

--- Miscellaneous/gdb/gdbSyntaxHelper.sh
+++ Miscellaneous/gdb/gdbSyntaxHelper.sh
@@ -1,10 +1,11 @@
 #Master gdb starting command
 gdb -q -x v6gdbInit.gdb --args ./myProgram arg1 arg2 
 
-
 #passing arguments
 gdb --args ./myProgram arg1 arg2
 
+#Analyze core without binary name
+gdb -c core.335343
 
 #Run default commands at startup
 gdb -x v6gdbInit.gdb --args ./myProgram arg1 arg2
