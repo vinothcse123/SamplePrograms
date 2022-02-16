35bfb8c344ab2936084c604fce5a3aba82665c87
Vinoth Energetic <vinothcse2000@gmail.com>
2022-02-16 19:17:32 +0530

    Update Exception.sql

--- PL-SQL/Exception.sql
+++ PL-SQL/Exception.sql
@@ -2,6 +2,8 @@ declare
 
 
 v_dummy number:=1;
+v_errm  VARCHAR2(64);
+v_query VARCHAR2(2000);
 
 begin
 
@@ -27,6 +29,10 @@ END LOOP;
 EXCEPTION
 
 WHEN OTHERS THEN
+DBMS_OUTPUT.PUT_LINE('Exception raised');
+v_code := SQLCODE;
+v_errm := SUBSTR(SQLERRM, 1, 64);
+DBMS_OUTPUT.PUT_LINE (v_code || ' ' || v_errm);
 NULL;
 end;
 
