f7e17fa5c8a4524ade0f9e12bc74b449c2f4c0dc
Vinoth Energetic <vinothcse2000@gmail.com>
2022-02-25 12:38:00 +0530

    Update TemplateSpecialization.cpp

--- cpp-concepts/TemplateSpecialization.cpp
+++ cpp-concepts/TemplateSpecialization.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 template <class T>
-class BaseClass 
+class TemplateClass 
 {
    public:
       template<typename MFT>
@@ -11,14 +11,28 @@ class BaseClass
       {              
 
       }
+
+      void templateMemberFuncDefileOutside(T a);
+      void templateMemberFuncSpecialization(T a);
+
 };
 
-template<typename T>
-void NonTemplateClass<T>::memberFunctionTempSpecializationN<string>(string a)
+template <class T>
+void TemplateClass<T>::templateMemberFuncDefileOutside(T a)
 {
 
 }
 
+template <class T>
+void TemplateClass<T>::templateMemberFuncSpecialization(T a)
+{
+
+}
+template <>
+void TemplateClass<std::string>::templateMemberFuncSpecialization(std::string a)
+{
+
+}
 
 class NonTemplateClass
 {
@@ -28,29 +42,33 @@ class NonTemplateClass
       {              
 
       }
-
 };
 
+// template<typename T>
+// void NonTemplateClass<T>::memberFunctionTempSpecializationN<string>(string a)
+// {
 
-void NonTemplateClass::memberFunctionTempSpecializationN<string>(string a)
-{
+// }
 
-}
+// void NonTemplateClass::memberFunctionTempSpecializationN<string>(string a)
+// {
 
-
-template <class c_Type1>
-template <class type1>
-type1 arithmetic_oper<c_Type1> ::  add(type1 a,type1 b)
-{
-   return a+b;
-}
+// }
 
 
-template <class c_Type1>
-c_Type1 arithmetic_oper<c_Type1> :: sub(c_Type1 a,c_Type1 b)
-{
-   return a-b;
-}
+// template <class c_Type1>
+// template <class type1>
+// type1 arithmetic_oper<c_Type1> ::  add(type1 a,type1 b)
+// {
+//    return a+b;
+// }
+
+
+// template <class c_Type1>
+// c_Type1 arithmetic_oper<c_Type1> :: sub(c_Type1 a,c_Type1 b)
+// {
+//    return a-b;
+// }
 
 template<class T>
 void callMemberFunctionOfUnknownType(T type)
@@ -59,10 +77,17 @@ void callMemberFunctionOfUnknownType(T type)
    type.template add(5,10);
 }
 
+void memberFunctionSpecializationOfTemplateClass()
+{
+    TemplateClass<int> a;
+    a.templateMemberFuncDefileOutside(10);
+    
+}
 
 
 int main()
 {
+    memberFunctionSpecializationOfTemplateClass();
 
    return 0;
 }
