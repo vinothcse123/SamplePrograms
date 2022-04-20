6a249a1c95914b73356538426675c1ee7b65441b
Vinoth Energetic <vinothcse2000@gmail.com>
2022-04-20 17:17:54 +0530

    Create TemplateCppFile2.cpp

---
+++ cpp-concepts/templateFileStructure/TemplateCppFile2.cpp
@@ -0,0 +1,11 @@
+
+#include<TemplateHeaderFile.h>
+
+namespace TemplateNameSpace
+{
+    void hello()
+    {
+        templateFuncSpecialization(86);
+        templateFuncSpecialization("HAI");
+    }
+}
