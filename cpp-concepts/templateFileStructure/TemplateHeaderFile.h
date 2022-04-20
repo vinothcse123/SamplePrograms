cf94ae7fe7a5f9395294859d7a571a54bc58986a
Vinoth Energetic <vinothcse2000@gmail.com>
2022-04-20 17:18:39 +0530

    Update TemplateHeaderFile.h

--- cpp-concepts/templateFileStructure/TemplateHeaderFile.h
+++ cpp-concepts/templateFileStructure/TemplateHeaderFile.h
@@ -1,15 +1,22 @@
 #pragma once
 
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-template <class type1>
-class TemplateClass 
+namespace TemplateNameSpace
 {
+
+   template <class T>
+   class TemplateClass
+   {
    public:
+      T add(T, T);
+   };
 
-      type1 add(type1,type1);
-};
+   template <class T>
+   void templateFuncSpecialization(T a);
+   template <>
+   void templateFuncSpecialization<std::string>(std::string a);
 
-#include<TemplateDefinitionFile.tpp> // Good practice to put in end of the header file
+}
+#include <TemplateDefinitionFile.tpp> // Good practice to put in end of the header file
