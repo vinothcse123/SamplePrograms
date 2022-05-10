845a230e9dbb75a7319a467e1ec749f0cfb0601a
Vinoth Energetic <vinothcse2000@gmail.com>
2022-05-10 16:42:55 +0530

    Update myMakeFile.mk

--- Miscellaneous/makeDemo/myMakeFile.mk
+++ Miscellaneous/makeDemo/myMakeFile.mk
@@ -1,12 +1,24 @@
 CCC=g++
-OBJ_FILES=myCppFile.o
-DEPENDENCY_RULE_FILES=myCppFile.d
+ALL_CPP_SOURCE_FILES=$(wildcard src/*.cpp)
+BUILD_DIR=./debug/
+SOURCE_DIR=./src/
+BIN_DIR=./bin/
+OBJ_FILES=$(addprefix $(BUILD_DIR)/, $(notdir $(ALL_CPP_SOURCE_FILES:.cpp=.o)))
+DEPENDENCY_RULE_FILES=$(addprefix $(BUILD_DIR)/, $(notdir $(ALL_CPP_SOURCE_FILES:.cpp=.d)))
 
 
 #Includes all dependency rules(.d files) generated using -MM command
 -include $(OBJ_FILES:.o=.d) 
 
-%.d:%.cpp
+logDetails:
+	@echo "Generating dependency rules and redirecting to .d file"
+
+	@echo "List of object files : $(OBJ_FILES)"
+
+	@echo "List of all CPP source files : $(ALL_CPP_SOURCE_FILES)"
+
+
+$(BUILD_DIR)/%.d:$(SOURCE_DIR)/%.cpp
 
 	@set -e; rm -f $@; \
 
@@ -16,18 +28,18 @@ DEPENDENCY_RULE_FILES=myCppFile.d
 	@$(CCC) -MM $< > $*.d.tmp
 
 	@#sed command is used here to make .d files depend on the any of the file change in cpp file For example: myFile.o myFile.d : myHeaderFile.h
-	@sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $*.d.tmp > $*.d; \
+	@sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $*.d.tmp > $(BUILD_DIR)/$*.d; \
 
 	#remove temp .d files
 	@rm -f $*.d.tmp
 
 
-
-%.o:%.cpp
-	@echo "Generating dependency rules and redirecting to .d file"
-	
+#Hey compiler, if you want to generate object files, refer respective cpp files and execute the below commands
+$(BUILD_DIR)/%.o:$(SOURCE_DIR)/%.cpp
 	@#compiling individual files
-	$(CCC) -c $< 
+	@echo $<
+	@echo $@
+	$(CCC) -c $< -o $@
 
 clean:
 	rm *.o *.d
@@ -35,8 +47,8 @@ clean:
 .PHONY: all clean
 
 #Starting point of make
-all: $(DEPENDENCY_RULE_FILES) $(OBJ_FILES)
-	@$(CCC) -o myBinaryFile $(OBJ_FILES)
+all: logDetails $(DEPENDENCY_RULE_FILES) $(OBJ_FILES)
+	@$(CCC) -o $(BIN_DIR)/myBinaryFile $(OBJ_FILES)
 
 
 
