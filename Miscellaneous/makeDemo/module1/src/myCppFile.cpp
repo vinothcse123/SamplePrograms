96e166c27135b39bc974f9383a86063e5580c448
Vinoth Energetic <vinothcse2000@gmail.com>
2022-05-13 11:57:19 +0530

    Create myCppFile.cpp

---
+++ Miscellaneous/makeDemo/module1/src/myCppFile.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::vector;
+using std::cout;
+using std::string;
+
+void demoFunction1()
+{
+
+}
+
+int main()
+{
+    int myNumber=-1;
+
+    demoFunction1();
+
+    cout<< "Result :" << myNumber << '\n';
+    return 0;
+}
