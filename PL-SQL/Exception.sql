adadfdb086083e230058439d3414c4ba0797a14e
Vinoth Energetic <vinothcse2000@gmail.com>
2022-05-25 19:42:25 +0530

    Update Exception.sql

--- PL-SQL/Exception.sql
+++ PL-SQL/Exception.sql
@@ -1,38 +1,23 @@
-declare
-
-
-v_dummy number:=1;
-v_errm  VARCHAR2(64);
-v_query VARCHAR2(2000);
-
-begin
-
-WHILE (v_dummy > 0)
-LOOP
-  
+DECLARE
+  v_dummy NUMBER:=1;
+  v_errm  VARCHAR2(64);
+  v_query VARCHAR2(2000);
 BEGIN
-select COUNT(*)
-Into v_dummy
-from V6D_DUPCHECK;
-
-
-Exception 
-  when no_data_found then
-Exit;
-END;
-
- dbms_output.put_line('EQUAL');
-
-    
-END LOOP;
-
+  WHILE (v_dummy > 0)
+  LOOP
+    BEGIN
+      SELECT COUNT(*) INTO v_dummy FROM V6D_DUPCHECK;
+    EXCEPTION
+    WHEN no_data_found THEN
+      EXIT;
+    END;
+    dbms_output.put_line('EQUAL');
+  END LOOP;
 EXCEPTION
-
 WHEN OTHERS THEN
-DBMS_OUTPUT.PUT_LINE('Exception raised');
-v_code := SQLCODE;
-v_errm := SUBSTR(SQLERRM, 1, 64);
-DBMS_OUTPUT.PUT_LINE (v_code || ' ' || v_errm);
-NULL;
-end;
-
+  DBMS_OUTPUT.PUT_LINE('Exception raised');
+  v_code := SQLCODE;
+  v_errm := SUBSTR(SQLERRM, 1, 64);
+  DBMS_OUTPUT.PUT_LINE (v_code || ' ' || v_errm);
+  NULL;
+END;
