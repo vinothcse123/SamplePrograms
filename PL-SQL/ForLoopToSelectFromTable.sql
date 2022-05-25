b34b22fe95fea3d1795be1fcbbdc23dff9c23bf5
Vinoth Energetic <vinothcse2000@gmail.com>
2022-05-25 20:47:01 +0530

    Delete ForLoopToSelectFromTable.sql

--- PL-SQL/ForLoopToSelectFromTable.sql
+++
@@ -1,5 +0,0 @@
-for i in (select PP.POSITION_ID  from HOLDINGDBO.POSITION PP where entity_id like 'NQNEWI01' and effective_date='3-Jan-2005')
-loop
-  DBMS_OUTPUT.PUT_LINE(i.position_id);
-  end loop;
-end;
