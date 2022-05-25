d841da3c7ac9593681718baae6b9f2bd0e079218
Vinoth Energetic <vinothcse2000@gmail.com>
2022-05-25 20:58:14 +0530

    Update UtilityQueries.sql

--- Database/oracle/UtilityQueries.sql
+++ Database/oracle/UtilityQueries.sql
@@ -1,32 +1,49 @@
 
 --H:OBJECTS
 
-SELECT * FROM ALL_OBJECTS; 
+SELECT * FROM ALL_OBJECTS WHERE UPPER(OBJECT_NAME) LIKE UPPER('%tableName%'); 
 
 --Package to get table definition
-select dbms_metadata.get_ddl('TABLE', 'TABLE_NAME') from user_tables;
+select dbms_metadata.get_ddl('TABLE', upper('TABLE_NAME_IN_CAPS')) from user_tables;
 
+--H:Database
+
+select * from tableName@SCHEMAL4;
+select * from tableName@SCHEMAL4;
+
+SELECT * FROM DBA_DB_LINKS WHERE DB_LINK LIKE 'VINO%';
+
+SELECT * FROM ALL_DB_LINKS WHERE DB_LINK LIKE 'VINOLEG%';
+
+SELECT * FROM USER_DB_LINKS WHERE DB_LINK LIKE 'VINO%';
 
 --H:Table
 
 select * from all_tables where  table_name LIKE '%ddd%';
 
-select * from all_synonyms where  synonym_name LIKE UPPER('%ddd%');
+select * from all_synonyms where  synonym_name LIKE UPPER('%dfd%');
 
-select * from all_mviews where  lower(mview_name) LIKE lower('%ddd%');
+select * from all_mviews where  lower(mview_name) LIKE lower('%tableName%');  
 
+select * from all_views where  lower(view_name) LIKE lower('%ALL_INDEXES%');  
 
+select * from ALL_PART_TABLES where table_name = 't1';
 
 --H:column
 
 --columns of the table
-select * from all_tab_columns where TABLE_NAME LIKE '%ALL_INDEX%' AND COLUMN_NAME LIKE '%STATUS%';
+select * from all_tab_columns where TABLE_NAME LIKE UPPER('%all_indexes%') AND COLUMN_NAME LIKE '%%' AND OWNER LIKE '%%' ORDER BY COLUMN_ID;
 
 --constraints of table
-select * from all_constraints where lower(table_name) like lower('ddd');
+select * from all_constraints where lower(table_name) like lower('all_indexes');
+
+
+--H:constraints
 
 --column information of constraint
- select * from all_cons_columns where lower(constraint_name) like lower('dd');
+ select * from all_cons_columns where lower(constraint_name) like lower('AVCON_34567_67891');
+
+ALTER TABLE VINO_KIR_TABLE_KEY DISABLE CONSTRAINT ddf;
 
 
 --Column related characteritics
@@ -53,37 +70,102 @@ JOIN all_constraints c_pk
 ON c.r_owner            = c_pk.owner
 AND c.r_constraint_name = c_pk.constraint_name
 Where C.Constraint_Type = 'R'
-AND A.Table_Name        like '%dd%';
+AND A.Table_Name        like '%VINO_KIR_TABLE_KEY%';
 
 
---H:session
+--H:Procedure
 
-select * from v$session;
+select * from all_source where name LIKE UPPER('%F_GET_KIR_PAWANS%') order by type, line;
 
 
---statistics of session
-select * from v$mystat;
+--H:DBA
+--:session
+--connection
+--Dynamic performance views(v$)
+
+SELECT * FROM v$version;
+
+
+-- Number of active connections
+
+select count(*) from v$session;
+
+
+-- Number of active connections allowed 
+SELECT name, value 
+  FROM v$parameter
+ WHERE name = 'sessions';
+
+
+
+-- For the given user, select value in column PROFILE,
+SELECT * FROM dba_users WHERE USERNAME LIKE 'NEW1121382%' AND PROFILE <> 'DEFALT';
+
+-- For connection timeout, resource name is IDLE_TIME
+select * from DBA_PROFILES where PROFILE LIKE 'DEFAU%';
+
+--statistics of current session
+select * from v$mystat where statistic# = 52;
+
+--List of all statistics
+select * from V$STATNAME where lower(name) like '%physical%';
+
+select * from V$SESSTAT;
+
+select * from V$SYSSTAT;
 
 
 --H:index
 
-SELECT STATUS,IDX.* FROM all_indexes IDX WHERE TABLE_NAME LIKE 'DD' AND TABLE_OWNER LIKE '%DDDD%';
+SELECT STATUS,IDX.* FROM all_indexes IDX WHERE TABLE_NAME LIKE UPPER('VINO_SFT_RNEW_RSFING_DATA') AND TABLE_OWNER LIKE 'NEW503412';
 
-SELECT IDX.* FROM all_ind_columns IDX WHERE INDEX_NAME LIKE '%DDD%' AND TABLE_OWNER LIKE '%DDD%';;
+SELECT IDX.* FROM all_ind_columns IDX WHERE INDEX_NAME LIKE 'ARS_VINO_INDX01' AND TABLE_OWNER LIKE 'NEW503412%';
+
+SELECT STATUS,T.* FROM DBA_IND_PARTITIONS T WHERE INDEX_NAME LIKE 'DDF_DD_BM_I' AND INDEX_OWNER LIKE 'NEW1157731'; 
+
+ANALYZE INDEX DDF_DD_BM_I COMPUTE STATISTICS;
 
 alter session set skip_unusable_indexes = true;
 
+alter index indexName rebuild;
+
 --H:ETC
 
---Refresh Materialized view
+--Refresh Materialized view mview
 
 begin
 -- f- fast refresh
-Dbms_Snapshot.Refresh( 'dd','f'); 
+Dbms_Snapshot.Refresh( 'PAWAN','f'); 
 End;
 /
+
 
 
 --H:PRIVILEGE
 
 SELECT * FROM DBA_SYS_PRIVS WHERE PRIVILEGE LIKE '%CONTEXT%';
+
+
+--H:policy
+
+select object_owner, policy_name,object_name as policy_applied_on, pf_owner, package, function
+from dba_policies
+where object_owner like 'ddd%';
+
+
+--H:Package
+
+--Retrive package body
+SELECT TEXT
+    FROM all_source
+   WHERE TYPE = 'PACKAGE BODY' AND name LIKE UPPER('%dd%')
+ORDER BY line;
+
+
+--H:Explain plan
+--H:Execution plan
+
+
+explain plan for select * from dual;
+
+select * from table(dbms_xplan.display);
