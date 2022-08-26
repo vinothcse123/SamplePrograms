38a920fd8cbd313e1e2511eb0907c40f241d424c
Vinoth Energetic <vinothcse2000@gmail.com>
2022-08-26 18:50:45 +0530

    Update Table.sql

--- Database/oracle/Table.sql
+++ Database/oracle/Table.sql
@@ -1,4 +1,29 @@
 
+--H:Table
+
+create table V6Play
+(
+MyNumber NUMBER,
+MyVarchar VARCHAR2(200),
+myDate date,
+myTimeStamp timestamp,
+MyNumber2 NUMBER
+);
+
+--:Primary key
+
+create table V6Play
+(
+MyNumber NUMBER,
+PRIMARY KEY(MyNumber)
+);
+
+ALTER TABLE V6Play add MyNumber3 NUMBER;
+
+DELETE FROM V6Play;
+SELECT * FROM V6Play;
+INSERT INTO V6Play(MyNumber,MYDATE) VALUES(10,to_date('10-DEC-21 10:34:33','yyyy/mm/dd hh24:mi:ss'));
+
 --Global Temporary table
 
 -- Temporary table will not be dropped at end of session, where as data inserted in one session is not visible to others.
