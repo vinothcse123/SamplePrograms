f08cdac857ee4cfb02370d2660c6e5c868b2c705
Vinoth Energetic <vinothcse2000@gmail.com>
2022-10-19 12:20:34 +0530

    Create main.cpp

---
+++ DevTools/gcov/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using std::vector;
+using std::cout;
+using std::string;
+
+void demoFunction1()
+{
+    
+}
+
+int main()
+{
+    int myNumber=-1;
+
+
+    return 0;
+}
