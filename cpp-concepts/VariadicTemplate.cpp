227135b359474504f7949c35fba662b71375fea5
Vinoth Energetic <vinothcse2000@gmail.com>
2023-02-21 06:22:30 +0530

    Update VariadicTemplate.cpp

--- cpp-concepts/VariadicTemplate.cpp
+++ cpp-concepts/VariadicTemplate.cpp
@@ -1,14 +1,15 @@
-//GH_CPP_CON_17
 
 #include<iostream>
 #include<string>
+#include<tuple>
+#include<vector>
 
 //Utility function
 template<typename T>
 T variadicTemplateFuncDemo(T myVar)
 {
     //std::cout << " SINGLE: "<< __PRETTY_FUNCTION__ << "\n";
-    std::cout << " VariadicUtilityFunction : " <<myVar<<std::endl;
+    std::cout << " variadicTemplateFuncDemo(SingleArgument) : " <<myVar<<std::endl;
     return  0;
 }
 
@@ -19,10 +20,53 @@ T variadicTemplateFuncDemo(T a,VariableArgsType... myVariableArgs)
     std::cout << "VariadicTemplateBeginning..." << a << "\n";
     //std::cout << " MAIN: "<< __PRETTY_FUNCTION__ << "\n";
     
-    std::cout << "VariadicTemplateFunction : "<< variadicTemplateDemo(myVariableArgs...) << std::endl;
+    std::cout << "variadicTemplateFuncDemo(ParameterPack) : "<< variadicTemplateFuncDemo(myVariableArgs...) << std::endl;
     return 99999;
 }
 
+template<typename T,typename ...VariableArgsType>
+T variadicTemplateFuncStoreInTupleDemo(T a,VariableArgsType... myVariableArgs)
+{
+    std::tuple<VariableArgsType...> myVariableArgsTuple(myVariableArgs...);
+        // std::tuple<Pack...> store( p... );
+
+    std::cout << "variadicTemplateFuncStoreInTupleDemo : "<< std::get<0>(myVariableArgsTuple) << std::endl;
+
+}
+
+
+
+//Utility function
+template<typename T>
+std::vector<T> variadicTemplateFuncStoreInVector(T myVar)
+{
+    std::vector<T> myIntVector;
+
+    myIntVector.push_back(myVar);
+
+    std::cout << " variadicTemplateFuncStoreInVector(SingleArgument) : " <<myVar<<std::endl;
+    return  myIntVector;
+}
+
+template<typename T,typename ...VariableArgsType>
+std::vector<T> variadicTemplateFuncStoreInVector(T a,VariableArgsType... myVariableArgs)
+{
+    std::vector<T> myIntVector=variadicTemplateFuncStoreInVector(myVariableArgs...);
+
+    myIntVector.push_back(a);
+
+    return myIntVector;
+}
+
+void variadicTemplateFuncStoreInVectorDriver()
+{
+    variadicTemplateFuncStoreInVector(100000,90909,20,909,998);
+
+    for(auto x : variadicTemplateFuncStoreInVector(100000,90909,20,909,998))
+    {
+        std::cout << " variadicTemplateFuncStoreInVector(Final) : " <<x<<std::endl;
+    }
+}
 
 int main()
 {	
@@ -30,6 +74,9 @@ int main()
 
     variadicTemplateFuncDemo(100000,10,20,909,998);
 
-	
+    variadicTemplateFuncStoreInTupleDemo(100000,90909,20,909,998);
+
+	variadicTemplateFuncStoreInVectorDriver();
+
 	return 0;
-} 
+}
