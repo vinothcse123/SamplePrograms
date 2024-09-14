1b622b509eb77c18e848b702ecdb5339119533cc
Vinoth Energetic <vinothcse2000@gmail.com>
2024-09-14 07:46:40 +0530

    Update stringFoundationalCode.cpp

--- cpp-concepts/FoundationalCode/stringFoundationalCode.cpp
+++ cpp-concepts/FoundationalCode/stringFoundationalCode.cpp
@@ -4,15 +4,23 @@
 
 void manipualteString()
 {
-	std::string manip="VINOTH KUMAR";
+	std::string manip="VINOTHKUMAR";
 	
-	manip.insert(1,"2"); 
-	std::cout << manip << std::endl;
-	manip.erase(2,3); 
-	manip.substr(1,5);
-	manip.append("GANESAN");
 	manip.length();
-	manip.find("GAN",0);
+	
+	manip.insert(1,"2"); //V2INOTHKUMAR
+	//std::cout << "Insert: " << manip << std::endl;
+	
+	manip.find("INO",0);
+	//std::cout << "Find: " << manip.find("INO",0) << std::endl;
+	
+	manip.erase(2,3); //V2THKUMAR
+	//std::cout << "Erase: " << manip << std::endl;
+
+	manip.substr(1,5);//2THKU
+	//std::cout << "Substr: " << manip.substr(1,5) << std::endl; // 2THKU
+	
+	manip.append("GANESAN");
 	
 	std::string concated=manip+std::string("KARUR");
 }
